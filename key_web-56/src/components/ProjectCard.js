cc84e6f90148a275be27e4abe657cf297636301c
pyrocoder <pyro@bk.ru>
2019-09-08 20:30:47 +0300

    Add files via upload

---
+++ key_web-56/src/components/ProjectCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ProjectCard(props) {
+    return (
+        <div className="project-card">
+            <img src={props.contact.imgUrl}/>
+            <h3>{props.contact.name}</h3>
+            <p>Phone: {props.contact.phone}</p>
+            <p>Email: {props.contact.email}</p>
+        </div>
+    )
+}
+
+export default ProjectCard
+
